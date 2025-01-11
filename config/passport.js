1812f1e67de0f87922a31035a69c6bb92e4aea45
abdulrahmanraziq <abdulrahmanraziq548@gmail.com>
2025-01-11 14:46:40 +0530

    server commit

---
+++ config/passport.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const User = require("../models/user.model");
+const Token = require("../models/token.model");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
+
+console.log('loose:', process.env.SECRET)
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET, // Loaded from .env
+};
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await User.findOne({ email: jwt_payload.email });
+      if (!user) return done(null, false);
+
+      const refreshTokenFromDB = await Token.findOne({ user: user._id });
+      if (!refreshTokenFromDB) return done(null, false);
+
+      const refreshPayload = jwt.verify(
+        refreshTokenFromDB.refreshToken,
+        process.env.REFRESH_SECRET
+      );
+
+      if (refreshPayload.email !== jwt_payload.email) return done(null, false);
+
+      const tokenExpiration = new Date(jwt_payload.exp * 1000);
+      const now = new Date();
+      const timeDifference = tokenExpiration.getTime() - now.getTime();
+
+      if (timeDifference > 0 && timeDifference < 30 * 60 * 1000) {
+        const payloadNew = { _id: user._id, email: user.email };
+        const newToken = jwt.sign(payloadNew, process.env.SECRET, {
+          expiresIn: "6h",
+        });
+        return done(null, { user, newToken });
+      }
+
+      return done(null, { user });
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+module.exports = passport;
